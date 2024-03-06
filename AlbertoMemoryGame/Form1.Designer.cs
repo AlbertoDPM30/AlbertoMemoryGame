5583ef7b7129e0541b2704987c42a47cad894c97
AlbertoDPM30 <albertodpm2000@gmail.com>
2024-03-05 23:18:45 -0400

    v1.0.0 full game

---
+++ AlbertoMemoryGame/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AlbertoMemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.btnClosed = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.scoreCount = new System.Windows.Forms.Label();
+            this.scoreCountNum = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.Color.Transparent;
+            this.gamePanel.Location = new System.Drawing.Point(12, 178);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(676, 510);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.gamePanel_Paint);
+            // 
+            // btnClosed
+            // 
+            this.btnClosed.BackColor = System.Drawing.Color.Red;
+            this.btnClosed.FlatAppearance.BorderSize = 0;
+            this.btnClosed.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClosed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClosed.Location = new System.Drawing.Point(625, 0);
+            this.btnClosed.Name = "btnClosed";
+            this.btnClosed.Size = new System.Drawing.Size(75, 33);
+            this.btnClosed.TabIndex = 1;
+            this.btnClosed.Text = "X";
+            this.btnClosed.UseVisualStyleBackColor = false;
+            this.btnClosed.Click += new System.EventHandler(this.btnClosed_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnReset.FlatAppearance.BorderColor = System.Drawing.Color.Purple;
+            this.btnReset.FlatAppearance.BorderSize = 2;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Noto Mono", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(511, 114);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(168, 39);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset Game";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Alberto\'s Memory Game";
+            // 
+            // scoreCount
+            // 
+            this.scoreCount.AutoSize = true;
+            this.scoreCount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.scoreCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scoreCount.Font = new System.Drawing.Font("Noto Mono", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreCount.Location = new System.Drawing.Point(12, 121);
+            this.scoreCount.Name = "scoreCount";
+            this.scoreCount.Size = new System.Drawing.Size(96, 25);
+            this.scoreCount.TabIndex = 4;
+            this.scoreCount.Text = "Record:";
+            // 
+            // scoreCountNum
+            // 
+            this.scoreCountNum.AutoSize = true;
+            this.scoreCountNum.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.scoreCountNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scoreCountNum.Font = new System.Drawing.Font("Noto Mono", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreCountNum.Location = new System.Drawing.Point(112, 121);
+            this.scoreCountNum.Name = "scoreCountNum";
+            this.scoreCountNum.Size = new System.Drawing.Size(24, 25);
+            this.scoreCountNum.TabIndex = 5;
+            this.scoreCountNum.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AlbertoMemoryGame.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(700, 700);
+            this.Controls.Add(this.scoreCountNum);
+            this.Controls.Add(this.scoreCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnClosed);
+            this.Controls.Add(this.gamePanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+        private System.Windows.Forms.Button btnClosed;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label scoreCount;
+        private System.Windows.Forms.Label scoreCountNum;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AlbertoMemoryGame/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AlbertoMemoryGame
+{
+    public partial class Form1 : Form
+    {
+
+        int colRowSize = 4;
+        int movements = 0;
+        int cardsBackQuantity = 0;
+        List<string> numberedCards;
+        List<string> scrambledCards;
+        ArrayList selectedCards;
+        PictureBox tempCard1;
+        PictureBox tempCard2;
+        int cardActual;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            startGame();
+        }
+
+        public void startGame()
+        {
+            timer1.Enabled = false;
+            timer1.Stop();
+            scoreCountNum.Text = "0";
+            cardsBackQuantity = 0;
+            movements = 0;
+            gamePanel.Controls.Clear();
+            numberedCards = new List<string>();
+            scrambledCards = new List<string>();
+            selectedCards= new ArrayList();
+
+            for (int i = 0; i <= 8; i++)
+            {
+                numberedCards.Add(i.ToString());
+                numberedCards.Add(i.ToString());
+            }
+
+            var randomNumber = new Random();
+            var result = numberedCards.OrderBy(item => randomNumber.Next());
+
+            foreach (string cardValue in result)
+            {
+                scrambledCards.Add(cardValue);
+            }
+
+            var panelTable = new TableLayoutPanel();
+            panelTable.RowCount = colRowSize;
+            panelTable.ColumnCount = colRowSize;
+
+            for (int i = 0; i < colRowSize; i++)
+            {
+                var porcentage = 150f / (float)colRowSize - 10;
+                panelTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, porcentage));
+                panelTable.RowStyles.Add(new RowStyle(SizeType.Percent, porcentage));
+            }
+
+            int recordsCounter = 1;
+
+            for (var i = 0; i < colRowSize; i++)
+            {
+                for (var j = 0; j < colRowSize; j++)
+                {
+                    var cardsGame = new PictureBox();
+                    cardsGame.Name = string.Format("{0}", recordsCounter);
+                    cardsGame.Dock = DockStyle.Fill;
+                    cardsGame.SizeMode = PictureBoxSizeMode.StretchImage;
+                    cardsGame.Image = Properties.Resources.cardBack;
+                    cardsGame.Cursor = Cursors.Hand;
+                    cardsGame.Click += btnCard_Click;
+                    panelTable.Controls.Add(cardsGame, j, i);
+                    recordsCounter++;
+                }
+            }
+
+            panelTable.Dock = DockStyle.Fill;
+            gamePanel.Controls.Add(panelTable);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int TimeBackCard = 1;
+            if (TimeBackCard == 1)
+            {
+                tempCard1.Image = Properties.Resources.cardBack;
+                tempCard2.Image = Properties.Resources.cardBack;
+                selectedCards.Clear();
+                TimeBackCard = 0;
+                timer1.Stop();
+            }
+        }
+
+        private void gamePanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnClosed_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCard_Click(object sender, EventArgs e)
+        {
+            if (selectedCards.Count < 2)
+            {
+                movements++;
+                scoreCountNum.Text = Convert.ToString(movements);
+                var selectedUserCards = (PictureBox)sender;
+
+                cardActual = Convert.ToInt32(scrambledCards[Convert.ToInt32(selectedUserCards.Name) - 1]);
+                selectedUserCards.Image = recoverImage(cardActual);
+                selectedCards.Add(selectedUserCards);
+
+                if (selectedCards.Count == 2)
+                {
+                    tempCard1 = (PictureBox)selectedCards[0];
+                    tempCard2 = (PictureBox)selectedCards[1];
+                    int card1 = Convert.ToInt32(scrambledCards[Convert.ToInt32(tempCard1.Name) - 1]);
+                    int card2 = Convert.ToInt32(scrambledCards[Convert.ToInt32(tempCard2.Name) - 1]);
+
+                    if (card1 != card2)
+                    {
+                        timer1.Enabled = true;
+                        timer1.Start();
+                    }
+                    else
+                    {
+                        cardsBackQuantity++;
+                        if (cardsBackQuantity > 7)
+                        {
+                            MessageBox.Show("El Juego Terminó");
+                        }
+                        tempCard1.Enabled = false;
+                        tempCard2.Enabled = false;
+                        selectedCards.Clear();
+                    }
+                }
+            }
+        }
+
+        public Bitmap recoverImage(int imageNumber) {
+            Bitmap tmpImg = new Bitmap(200, 100);
+            switch (imageNumber)
+            {
+                case 0: tmpImg = Properties.Resources.cardBack;
+                    break;
+                default: tmpImg = (Bitmap)Properties.Resources.ResourceManager.GetObject("_" + imageNumber);
+                    break;
+            }
+            return tmpImg;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            startGame();
+        }
+
+    }
+}
